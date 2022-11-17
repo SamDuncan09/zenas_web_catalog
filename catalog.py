ead021ebf881476b841d0b22825bc501c6e15c00
Sam Duncan <117125540+SamDuncan09@users.noreply.github.com>
2022-11-17 20:21:33 +0000

    Update catalog.py
    added code

--- catalog.py
+++ catalog.py
@@ -3,3 +3,14 @@ import streamlit
 streamlit.title('Zenas Online Catalog')
 
 import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+
+my_cur = my_cnx.cursor ()
+
+my_cur.execute("select current_user(), current_account(), current_region()")
+
+my_data_row = my_cur.fetchone()
+
+streamlit.txt("Hello from Snowflake:")
+streamlit.txt(my_data_row)
